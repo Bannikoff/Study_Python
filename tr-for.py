5ecb76299cba39a2b1fe0e005ea81d640ce5223f
Bannikoff <klerins@gmail.com>
2024-02-29 11:02:18 +0100

    Дізнався про max і вийшло все вирішити в один рядок.

--- tr-for.py
+++ tr-for.py
@@ -1,6 +1,11 @@
-word = 'Magnification'
-string = ''
-for i in range (len(word)-1, -1, -1):
-  string += word[i]
-  # print (word[i])
-print (string)
+# word = 'Magnification'
+# string = ''
+# for i in range (len(word)-1, -1, -1):
+#   string += word[i]
+#   # print (word[i])
+# print (string)
+
+
+def pillars(num_pill, dist, width):
+    return (num_pill-1) * dist * 100 + max(num_pill-2, 0) * width
+print (pillars (30, 10, 10))
