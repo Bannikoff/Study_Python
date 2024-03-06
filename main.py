e6a1a770f94d49e9c822c247a62e3daa936ae241
Bannikoff <klerins@gmail.com>
2024-03-06 18:44:54 +0100

    Учу встроенные функции. Функция max

--- main.py
+++ main.py
@@ -18,16 +18,5 @@
 # print ('Вгадав') if num == 55 else print ('Не вгадав')
 
 prices = [33, 49, 55, 14]
-
-# total price
-total = sum(prices)
-print("Total Price:", total)
-
-
-# number of products
-number_of_products = len(prices)
-print("Count:", number_of_products)
-
-# calculating the average
-avg_price = total/number_of_products
-print("Average Price:", avg_price)
+max_price = max(prices)
+print(max_price)
