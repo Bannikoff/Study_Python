107621ecd1e6b26290377552aa2bcbf14b1509df
Bannikoff <klerins@gmail.com>
2024-03-18 16:58:53 +0100

    # Вам потрібно створити функцію з назвою is_positive, яка отримує єдиний аргумент n. Вона повинна повертати # 'positive', якщо n більше 0; # 'negative', якщо n менше 0; # 'zero', якщо n дорівнює нулю

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
-# Це приклад скрипту на Python для PyCharm.
+# Вам потрібно створити функцію з назвою is_positive, яка отримує єдиний аргумент n. Вона повинна повертати
+# 'positive', якщо n більше 0;
+# 'negative', якщо n менше 0;
+# 'zero', якщо n дорівнює нулю
 
-# Натисніть ⌃R, щоб виконати його або замінити своїм кодом.
-# Подвійне натискання клавіші ⇧ для пошуку класів, файлів, вікон інструментів, дій та налаштувань.
-#
-#
-# def print_hi(name):
-#     # Для налагодження скрипта використовуйте точку переривання в рядку коду нижче.
-#         print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-#
-#
-# # Натисніть зелену кнопку в жолобі, щоб запустити скрипт.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
-#
-# # Див. довідку PyCharm на https://www.jetbrains.com/help/pycharm/
-# num = int(input('Number: '))
-# print ('Вгадав') if num == 55 else print ('Не вгадав')
+# Define a function
+def is_positive(___):
+  if ___:
+    return('positive')
+  elif n < 0:
+    return('negative')
+  ___:
+    return('___')
 
-def sum_array(arr):
-    if arr is None or len(arr) <= 1:
-        return 0
-    else:
-        arr.sort()  # Сортуємо список
-        arr.pop(-1)  # Видаляємо найбільший елемент
-        arr.pop(0)  # Видаляємо найменший елемент
-        return sum(arr)
+# Test your function on -5, 0 and 5
+print('Number -5 is', is_positive(-5))
+print('Number 0 is', is_positive(0))
+print('Number 5 is', is_positive(5))
